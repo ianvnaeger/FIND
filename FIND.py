409613590788e05557aaab822186308e385902b9
ianvnaeger <ianvnaeger@gmail.com>
2018-10-14 02:25:28 -0500

    might have SourceValidation working...

--- FIND.py
+++ FIND.py
@@ -9,6 +9,13 @@ import logging
 from flask import Flask
 from flask import request
 
+# import pymysql.cursors
+# Environment variables are defined in app.yaml.
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 app = Flask(__name__)
 
 @app.errorhandler(500)
@@ -109,6 +116,35 @@ def AuthorNoteriety( authorName ):
         
     return noteriety
 
+def SourceValidation(url):
+
+    url.replace("https://","")
+    url.replace("http://","")
+
+    endIndex = url.index("/")
+    endOfUrl = len(url) - 1
+    url[:-(endOfUrl - endIndex)]
+
+    httpUrl = 'http://' + url
+    httpsUrl = 'https://' + url
+
+    #connection = pymysql.connect(host='35.239.255.99', user='root', password='password', db='source_validation', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+    #cursor = db.cursor()
+    # db.Model.query.
+    for score in db.Model.query(urls).filter(or_(url==httpsUrl, url==httpUrl)):
+        print(score)
+        send = score
+
+    # with connection.cursor() as cursor:
+    #     sql = "SELECT `score` FROM `urls` WHERE `url` = %s OR `url` = %s"
+    #     cursor.execute(sql, (httpUrl, httpsUrl,))
+    #     score = cursor.fetchone()
+    #     print(score)
+    # connection.close()
+
+    return send
+
 def Decider():
     #given ratings from the classifier, use Machine Learning Magic to determine if fake news
     model = keras.models.Sequential()
@@ -171,9 +207,10 @@ def main():
     print(titlePolarity)
     print(dateRating)
     print('sourceRating')
+    return print(SourceValidation('https://www.cnet.com/news/google-plus-and-life-after-social-media-death/'))
     #x = Decider()
     # print("we did it")
-    
-    return 'success'
+
+    #return 'success'
 
 # main()
--- SourceValidation.py
+++ SourceValidation.py
@@ -1,15 +1,37 @@
-import MySQLdb
+import pymysql.cursors
+# Environment variables are defined in app.yaml.
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
 def SourceValidation(url):
 
     url.replace("https://","")
     url.replace("http://","")
 
-    url.index()
+    endIndex = url.index("/")
+    endOfUrl = len(url) - 1
+    url[:-(endOfUrl - endIndex)]
 
-    db = MySQLdb.connect("35.239.255.99","root","password","source_validation")
+    httpUrl = 'http://' + url
+    httpsUrl = 'https://' + url
 
-    cursor = db.cursor()
+    #connection = pymysql.connect(host='35.239.255.99', user='root', password='password', db='source_validation', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
-    sql = "select score from urls where url = /'" + httpUrl + "/' or url = /'" + httpsUrl + "/'"
+    #cursor = db.cursor()
+    # db.Model.query.
+    for score in db.Model.query(urls).filter(or_(url==httpsUrl, url==httpUrl)):
+        print(score)
+        send = score
+
+    # with connection.cursor() as cursor:
+    #     sql = "SELECT `score` FROM `urls` WHERE `url` = %s OR `url` = %s"
+    #     cursor.execute(sql, (httpUrl, httpsUrl,))
+    #     score = cursor.fetchone()
+    #     print(score)
+    # connection.close()
+
+    return send
+
+print(SourceValidation('https://www.cnet.com/news/google-plus-and-life-after-social-media-death/'))
